4c626de32ef26d922d602c9313e607f638cb98d9
Sun Yanlin <1310400098@qq.com>
2023-08-28 14:58:40 +0800

    - 更改文件结构 - 数据库小修改

---
+++ DAO/UserDao.java
@@ -0,0 +1,69 @@
+package view.DAO;
+
+import view.VObj.User;
+
+import java.sql.*;
+
+public class UserDao {
+
+    public User findUserByuId(String uId) {
+
+        String sqlString = "select * from tblUser where uId = '" + uId + "'";
+        User user = new User();
+
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///C:\\Users\\15222\\Desktop\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            ResultSet res = sta.executeQuery(sqlString);
+
+            if(!res.next()){
+                return null;
+            }
+            res.beforeFirst();
+
+            res.next();
+
+            user.setuId(res.getString(1));
+            user.setuPwd(res.getString(2));
+            user.setuRole(res.getString(3));
+
+            con.close();//关闭数据库连接
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public boolean CreateUser(User user){
+
+        String sqlString = "insert into tblUser(uId,uPwd,uRole) values('" + user.getuId() + "','" + user.getuPwd() + "','" + user.getuRole() + "')";
+
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///C:\\Users\\15222\\Desktop\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名 ，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            sta.executeUpdate(sqlString);
+
+            con.close();//关闭数据库连接
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return true;
+    }
+}
---
+++ UI/logInHandler.java
@@ -0,0 +1,96 @@
+package view.UI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import view.DAO.UserDao;
+import view.VObj.User;
+
+import javax.swing.*;
+
+public class logInHandler extends KeyAdapter implements ActionListener{
+    private logInUI loginView;
+    private boolean radioButtonSelected = false;
+    public logInHandler(logInUI loginView){
+
+        this.loginView=loginView;
+    }
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() instanceof JRadioButton) {
+            radioButtonSelected = true;
+        } else if (e.getSource() instanceof JButton) {
+            JButton jButton = (JButton) e.getSource();
+            String text2 = jButton.getText();
+            // 执行按钮操作
+            if ("注册".equals(text2)) {
+                loginView.dispose();
+                new RegisterUI();
+            }
+            else if (!radioButtonSelected) {
+                // 当单选按钮未被选择时，不执行后续的操作
+                JOptionPane.showMessageDialog(loginView, "请先选择单选按钮");
+                return;
+            }
+            if ("登录".equals(text2)) {
+                extracted();
+            }
+        }
+
+    }
+
+    private void extracted() {
+        String userId=loginView.getUserNameTxt().getText();
+        String password=loginView.getPwdField().getText();
+        if(userId.length()!=9){
+            JOptionPane.showMessageDialog(loginView,"请输入9位用户名！");
+            loginView.getUserNameTxt().setText("");
+            loginView.getPwdField().setText("");
+            return;
+        }
+
+        else if(password.length()==0)
+        {
+            JOptionPane.showMessageDialog(loginView,"请输入密码！");
+        }
+
+
+
+        //查询数据库，给flag值
+        boolean flag=false;
+
+        UserDao userdao = new UserDao();
+        User user = userdao.findUserByuId(userId);
+
+        String role = "";
+        if(loginView.studentRadioButton.isSelected())
+            role = "ST";
+        else if(loginView.teacherRadioButton.isSelected())
+            role = "TC";
+        else if(loginView.adminRadioButton.isSelected())
+            role = "AD";
+
+        if(user != null && user.getuPwd().equals(password) && user.getuRole().equals(role)){
+            flag = true;
+        }//判断登录信息是否正确
+
+
+        if(flag){
+            JOptionPane.showMessageDialog(loginView,"登录成功！");
+        }
+        else {
+            JOptionPane.showMessageDialog(loginView,"用户名或密码或用户类型错误！");
+        }
+
+        System.out.println(userId+":"+password+":"+role);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        e.getKeyCode();
+        if(KeyEvent.VK_ENTER==e.getKeyCode()){
+            extracted();
+        }
+    }
+}
