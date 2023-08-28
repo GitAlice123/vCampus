63fc8fb57b77ebcd021da321493b736996966dad
Sun Yanlin <1310400098@qq.com>
2023-08-28 15:50:05 +0800

    编写Book实体类

---
+++ VObj/Book.java
@@ -0,0 +1,103 @@
+package view.VObj;
+
+public class Book {
+    String bookId;
+    String bookISBN;
+    String author;
+    String bookType;
+    Double bookPrise;
+    String publisher;
+    String summary;
+    int totalNum;
+    int freeNum;
+    String bookPos;
+    int borrowNum;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public void setBookISBN(String bookISBN) {
+        this.bookISBN = bookISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBookType() {
+        return bookType;
+    }
+
+    public void setBookType(String bookType) {
+        this.bookType = bookType;
+    }
+
+    public Double getBookPrise() {
+        return bookPrise;
+    }
+
+    public void setBookPrise(Double bookPrise) {
+        this.bookPrise = bookPrise;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public int getTotalNum() {
+        return totalNum;
+    }
+
+    public void setTotalNum(int totalNum) {
+        this.totalNum = totalNum;
+    }
+
+    public int getFreeNum() {
+        return freeNum;
+    }
+
+    public void setFreeNum(int freeNum) {
+        this.freeNum = freeNum;
+    }
+
+    public String getBookPos() {
+        return bookPos;
+    }
+
+    public void setBookPos(String bookPos) {
+        this.bookPos = bookPos;
+    }
+
+    public int getBorrowNum() {
+        return borrowNum;
+    }
+
+    public void setBorrowNum(int borrowNum) {
+        this.borrowNum = borrowNum;
+    }
+}
