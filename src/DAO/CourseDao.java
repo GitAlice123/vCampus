e80b8947a53dd624a957f583b3e0b5eda4924a35
Sun Yanlin <1310400098@qq.com>
2023-08-30 11:11:03 +0800

    增加课程查询， 添加班级

--- src/DAO/CourseDao.java
+++ src/DAO/CourseDao.java
@@ -5,9 +5,8 @@ import view.CourseSelection.Course;
 import java.sql.*;
 
 public class CourseDao {
-    public Course findUserByuId(String uId) {
+    public Course findCourseByNum(String courseNum) {
 
-        String sqlString = "select * from tblCoures where uId = '" + uId + "'";
         Course course = new Course();
 
         try {
@@ -19,11 +18,18 @@ public class CourseDao {
             Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
             //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
             Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-            ResultSet res = sta.executeQuery(sqlString);
+            ResultSet res = sta.executeQuery("select * from tblCourse where courseId = '" + courseNum + "'");
 
             if(!res.next()){return null;}
             res.beforeFirst();
             res.next();
+
+
+            course.setCourseNum(res.getString(1));
+            course.setCourseName(res.getString(2));
+            course.setCourseType(res.getString(3));
+            course.setCourseTime(res.getDouble(4));
+            course.setCredit(res.getDouble(5));
 
 
 
@@ -33,6 +39,6 @@ public class CourseDao {
             e.printStackTrace();
         }
 
-        return user;
+        return course;
     }
 }
---
+++ src/SchoolRolls/Grade.java
@@ -0,0 +1,46 @@
+package view.SchoolRolls;
+
+public class Grade {
+    private String CardID;
+    private String courseID;
+
+    private double Grade;
+
+    public Grade(String cardID, String courseID, double grade) {
+        CardID = cardID;
+        this.courseID = courseID;
+        this.Grade = grade;
+    }
+    public String getCardID() {
+        return CardID;
+    }
+
+    public void setCardID(String cardID) {
+        CardID = cardID;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public double getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(double grade) {
+        this.Grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "CardID='" + CardID + '\'' +
+                ", courseID='" + courseID + '\'' +
+                ", Grade=" + Grade +
+                '}';
+    }
+}
