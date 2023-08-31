06ccb5c30927d41c68816db6fe92a5046f6af06e
GitAlice123 <2822983966@qq.com>
2023-08-31 11:03:23 +0800

    添加几个message类型

---
+++ src/message/NoDataReqMessage.java
@@ -0,0 +1,9 @@
+package view.message;
+/* 这个用于客户端只发送请求但不需要发送数据表单的情况 */
+public class NoDataReqMessage {
+    String ReqMessage;
+    public NoDataReqMessage() {
+        // 无参数构造函数
+        this.ReqMessage = "null";
+    }
+}
