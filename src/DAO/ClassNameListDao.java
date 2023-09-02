737aa29db31c0c953eed7fe6f4305f2f8557a402
Sun Yanlin <1310400098@qq.com>
2023-09-02 16:51:14 +0800

    创建学生和班级的映射

--- src/DAO/ClassNameListDao.java
+++ src/DAO/ClassNameListDao.java
@@ -71,4 +71,32 @@ public class ClassNameListDao {
         return classlist;
     }
 
+    public boolean createClassStudentLink(String classId,String uId){
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名 ，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet test = sta.executeQuery("select * from tblClassNameList where classId = '" + classId + "' and uId = '" + uId + "'");
+            if (test.next()) {
+                con.close();
+                return false;
+            }
+            sta.executeUpdate("insert into tblClassNameList(classId,uId) values('"
+                    + classId + "','"
+                    + uId + "')");
+
+            con.close();//关闭数据库连接
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
 }
