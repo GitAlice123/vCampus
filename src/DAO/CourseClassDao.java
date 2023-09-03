72ea38e50cb805d95a83e8f651dc76365b72e1a5
Sun Yanlin <1310400098@qq.com>
2023-09-03 17:47:00 +0800

    增加班级

--- src/DAO/CourseClassDao.java
+++ src/DAO/CourseClassDao.java
@@ -53,4 +53,40 @@ public class CourseClassDao {
         return classes;
     }
 
+    public boolean createClass(CourseClass courseClass){
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名 ，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet test = sta.executeQuery("select * from tblClass where classId = '" + courseClass.getClassID() + "'");
+            if (test.next()) {
+                con.close();
+                return false;
+            }
+            sta.executeUpdate("insert into tblClass(classId,courseId,classTeacher,classTeacherId,classPlace,classMax,classTemp,classTime) values('"
+                    + courseClass.getClassID() + "','"
+                    + courseClass.getCourseNum() + "','"
+                    + "Null" + "','"
+                    + courseClass.getClassTeacher() + "','"
+                    + courseClass.getClassPlace() + "','"
+                    + courseClass.getClassMax() + "','"
+                    + courseClass.getClassTemp() + "','"
+                    + courseClass.getClassTime() + "')");
+
+            con.close();//关闭数据库连接
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+
+
 }
