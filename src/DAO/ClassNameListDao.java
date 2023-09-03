d0b3e1492f96010715a190c651a8a0a70d8a03ff
Sun Yanlin <1310400098@qq.com>
2023-09-03 17:23:14 +0800

    学生退选

--- src/DAO/ClassNameListDao.java
+++ src/DAO/ClassNameListDao.java
@@ -71,7 +71,7 @@ public class ClassNameListDao {
         return classlist;
     }
 
-    public boolean createClassStudentLink(String classId,String uId){
+    public boolean createClassStudentLink(String classId, String uId) {
         try {
             Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
         } catch (ClassNotFoundException e) {
@@ -91,6 +91,30 @@ public class ClassNameListDao {
                     + uId + "')");
 
             con.close();//关闭数据库连接
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    public boolean deleteClassStudentLink(String classId, String uId) {
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名 ，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+            int res = sta.executeUpdate("delete from tblClassNameList where classId = '" + classId + "' and uId = '" + uId + "'");
+
+            con.close();//关闭数据库连接
+
+            if (res == 0) return false;
 
         } catch (SQLException e) {
             e.printStackTrace();
