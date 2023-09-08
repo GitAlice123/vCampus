651c8360dcc800970fab60acc42541575bf26ce1
Sun Yanlin <1310400098@qq.com>
2023-09-08 10:32:01 +0800

    時間修改

--- src/DAO/CourseClassDao.java
+++ src/DAO/CourseClassDao.java
@@ -257,5 +257,35 @@ public class CourseClassDao {
         return classes;
     }
 
+    public boolean ModifyClass(CourseClass courseClass) {
+        String sqlString = "update tblClass " +
+                "set courseId = " + courseClass.getCourseNum() +
+                "set classTeacher = " + courseClass.getCourseNum() +
+                "set classTeacherId = " + courseClass.getCourseNum() +
+                "set classPlace = " + courseClass.getCourseNum() +
+                "set classMax = " + courseClass.getCourseNum() +
+                "set classTemp = " + courseClass.getCourseNum() +
+                "set classTime = " + courseClass.getCourseNum() +
+                " where classId = '" + courseClass.getClassID() + "'";
 
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名 ，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            int count = sta.executeUpdate(sqlString);
+            if (count == 0) return false;
+
+            con.close();//关闭数据库连接
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
 }
