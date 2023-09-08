e69a2d99c893e0694b084c09f6f67184b0da296d
Sun Yanlin <1310400098@qq.com>
2023-09-08 09:41:43 +0800

    增加展示班级功能

--- src/DAO/CourseClassDao.java
+++ src/DAO/CourseClassDao.java
@@ -1,5 +1,6 @@
 package view.DAO;
 
+import view.CourseSelection.Course;
 import view.CourseSelection.CourseClass;
 
 import java.sql.*;
@@ -208,5 +209,51 @@ public class CourseClassDao {
             e.printStackTrace();
         }
         return true;
+    }
+
+    public CourseClass[] showAllClass() {
+        CourseClass[] classes = new CourseClass[1];
+
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet res = sta.executeQuery("select * from tblClass");
+
+            res.last();
+            int count = res.getRow();
+            res.beforeFirst();
+
+            if (count == 0) {
+                return null;
+            }
+
+            classes = new CourseClass[count];
+            int index = 0;
+            while (res.next()) {//不断的移动光标到下一个数据
+                classes[index] = new CourseClass(
+                        res.getString(1),
+                        res.getString(2),
+                        res.getString(4),
+                        res.getString(5),
+                        res.getInt(6),
+                        res.getInt(7),
+                        res.getString(8));
+                index++;
+            }
+
+            con.close();//关闭数据库连接
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return classes;
     }
 }
