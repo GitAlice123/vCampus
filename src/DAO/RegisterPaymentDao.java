fde211b1c443d2022289becb0cf53c423d35d900
Sun Yanlin <1310400098@qq.com>
2023-09-08 10:49:11 +0800

    展示全部挂號

--- src/DAO/RegisterPaymentDao.java
+++ src/DAO/RegisterPaymentDao.java
@@ -1,5 +1,6 @@
 package view.DAO;
 
+import view.CourseSelection.CourseClass;
 import view.Hospital.Register;
 
 import java.sql.*;
@@ -144,5 +145,48 @@ public class RegisterPaymentDao {
             e.printStackTrace();
         }
         return true;
+    }
+
+    public Register[] showAllRegister() {
+        Register[] regs = new Register[1];
+
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet res = sta.executeQuery("select * from tblRegisterPayment");
+
+            res.last();
+            int count = res.getRow();
+            res.beforeFirst();
+
+            if (count == 0) {
+                return null;
+            }
+
+            regs = new Register[count];
+            int index = 0;
+            while (res.next()) {//不断的移动光标到下一个数据
+                regs[index] = new Register(
+                        res.getString(1),
+                        res.getTimestamp(2),
+                        res.getString(3),
+                        res.getString(4),
+                        res.getBoolean(5),
+                        res.getDouble(6)
+                );
+                index++;
+            }
+            con.close();//关闭数据库连接
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return regs;
     }
 }
