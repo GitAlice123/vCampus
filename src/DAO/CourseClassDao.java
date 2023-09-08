70b22327e4f7981c6159089d6ec7f388ea652fe8
Sun Yanlin <1310400098@qq.com>
2023-09-08 09:34:52 +0800

    增加删除班级功能

--- src/DAO/CourseClassDao.java
+++ src/DAO/CourseClassDao.java
@@ -189,4 +189,24 @@ public class CourseClassDao {
         return classes;
     }
 
+    public boolean deleteClassByClassId(String classId){
+        try {
+            Class.forName("com.hxtt.sql.access.AccessDriver");//导入Access驱动文件，本质是.class文件
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection con = DriverManager.getConnection("jdbc:Access:///.\\src\\Database\\vCampus.mdb", "", "");
+            //与数据库建立连接，getConnection()方法第一个参数为jdbc:Access:///+文件总路径,第二个参数是用户名 ，第三个参数是密码（Access是没有用户名和密码此处为空字符串）
+            Statement sta = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            int res = sta.executeUpdate("delete from tblClass where classId = '" + classId + "'");
+
+            con.close();//关闭数据库连接
+            if (res == 0) return false;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 }
