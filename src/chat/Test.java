151f57ae9d5a6812d1396cc761693d688fcb081b
Sun Yanlin <1310400098@qq.com>
2023-09-10 14:16:02 +0800

    从main拷贝文心一言提交

--- src/chat/Test.java
+++ src/chat/Test.java
@@ -7,13 +7,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import iew.connect.ChatClientAPI;
-import view.conneimport view.message.ChatQuesMessage;
+import view.connect.ChatClientAPI;
+import view.connect.*;
+import view.message.ChatQuesMessage;
 
-public lass Tet {
+public class Test {
     public static void main(String[] args) throws IOException {
         ChatClientAPI chatClientAPI = new ChatClientAPIImpl("localhost",8888);
-        ChatQuesMessage chatQuesMessage = new ChatQuesMessage("帮我写一个Python代码，输出hello world");
+        ChatQuesMessage chatQuesMessage = new ChatQuesMessage("帮我解析一首李白的诗");
         String answer = chatClientAPI.getGPTAnswer(chatQuesMessage);
         System.out.println(answer);
     }
