32d98b8952a3d43ca0e4886b50ce4e96ca29633b
Sun Yanlin <1310400098@qq.com>
2023-09-14 03:27:07 +0800

    chat功能回滚

--- src/chat/ChatFrameUI.java
+++ src/chat/ChatFrameUI.java
@@ -1,5 +1,7 @@
 package view.chat;
 
+import view.Global.GlobalData;
+import view.Global.SummaryManagerUI;
 import view.Global.SummaryStudentTeacherUI;
 import view.client.ClientReceiver;
 import view.connect.ChatClientAPI;
@@ -7,6 +9,7 @@ import view.connect.ChatClientAPIImpl;
 import view.message.ChatMessage;
 import view.message.ChatQuesMessage;
 import view.message.ChatWithUserMessage;
+import view.message.UniqueMessage;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,15 +18,22 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.print.PrinterException;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 
 public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallback {
+    private Timer waitTimer; // 定时器
+    private Timer timer;
+    private int dotCount; // 点号数量
     SpringLayout springLayout = new SpringLayout();
     JPanel topPanel;
-
     JPanel centerPanel;
     JPanel bottomPanel;
-    // 底部
+
+    //底部
     JScrollPane bottompane;
     JTextArea textarea;
     JButton sendBtn;
@@ -32,43 +42,45 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
     ButtonGroup choose;
     JRadioButton AIassistant;
     JRadioButton schoolchat;
-    // 中部
+
+    //中部
     JScrollPane centerpane;
     JTextArea chathistory;
     JScrollPane leftpane;
     JTextArea online;
-    // 顶部
-    JLabel welcomeLabel;
-    Font buttonFont = new Font("楷体", Font.PLAIN, 25);// 设置按钮的文字大小、字体
-    Font centerFont = new Font("楷体", Font.PLAIN, 20);// 设置中间组件的文字大小、字体
 
-    // 颜色
+
+    //顶部
+    JLabel welcomeLabel;
+
+    Font buttonFont = new Font("楷体", Font.PLAIN, 25);//设置按钮的文字大小、字体
+    Font centerFont = new Font("楷体", Font.PLAIN, 20);//设置中间组件的文字大小、字体
+
+    //颜色
     Color bottomcolor = new Color(233, 208, 222);
     Color topcolor = new Color(152, 193, 202);
-
-    private Timer waitTimer; // 定时器
-    private int dotCount; // 点号数量
-
-    /**
-     * 存储发送出去的消息
-     */
     String messageSent;
 
-    /**
-     * 存储服务器发回的消息
-     */
     private String messageReturn; // 保存服务器返回的消息
 
-    /**
-     * 聊天框UI的构造函数
-     *
-     * @throws IOException
-     */
     public ChatFrameUI() throws IOException {
 
         super("ChatFrame");
         startReceivingMessages();
+//        startReceivingOnline();
 
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 每秒调用的函数
+                try {
+                    showAllOnlineList();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        startTimer();
         topPanel = new JPanel();
         centerPanel = new JPanel(springLayout) {
             @Override
@@ -95,13 +107,15 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
 
                 g2d.dispose();
             }
-        };
+        }
+        ;
         bottomPanel = new JPanel();
 
         topPanel.setBackground(topcolor);
         bottomPanel.setBackground(bottomcolor);
 
-        // 底部
+
+        //底部
         textarea = new JTextArea();
         sendBtn = new JButton("发送");
         backBtn = new JButton("退出");
@@ -130,7 +144,8 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
         bottomPanel.add(sendBtn);
         bottomPanel.add(backBtn);
 
-        // 中部
+
+        //中部
         chathistory = new JTextArea();
         chathistory.setLineWrap(true);
         chathistory.setWrapStyleWord(true);
@@ -147,12 +162,10 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
         leftpane = new JScrollPane(online);
         leftpane.setPreferredSize(new Dimension(200, 450));
 
-        // TODO:获得所有在线人员名单
-
         friends = new JComboBox<String>();
-        friends.addItem("薛沛林");
-        friends.addItem("余畅");
         friends.setFont(centerFont);
+        //showAllOnlineList();
+
 
         centerPanel.add(leftpane);
         centerPanel.add(centerpane);
@@ -164,7 +177,6 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
         springLayout.putConstraint(SpringLayout.NORTH, centerpane, 0, SpringLayout.NORTH, leftpane);
         springLayout.putConstraint(SpringLayout.WEST, friends, 20, SpringLayout.WEST, leftpane);
         springLayout.putConstraint(SpringLayout.NORTH, friends, 40, SpringLayout.SOUTH, leftpane);
-
         waitTimer = new Timer(500, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -180,23 +192,42 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
         sendBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    sendMessage();
-                } catch (PrinterException ex) {
-                    throw new RuntimeException(ex);
+//                AIassistant = new JRadioButton("AI助手");
+//                schoolchat = new JRadioButton("校内聊天");
+                if (AIassistant.isSelected()) {
+                    try {
+                        sendMessage();
+                    } catch (PrinterException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    receiveGPTMessage();
+                } else {
+                    try {
+                        sendMessage();
+                    } catch (PrinterException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    String selectedTarget = (String) friends.getSelectedItem();
+                    try {
+                        sendOthersMessage(selectedTarget);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 }
-                receiveGPTMessage();
             }
         });
         backBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
-                new SummaryStudentTeacherUI();
+                if (GlobalData.getUType() == 1 || GlobalData.getUType() == 2) {
+                    new SummaryStudentTeacherUI();
+                } else new SummaryManagerUI();
+
             }
         });
 
-        // 顶部
+        //顶部
         welcomeLabel = new JLabel("欢迎来到聊天室！");
         welcomeLabel.setFont(new Font("楷体", Font.PLAIN, 50));
         topPanel.add(welcomeLabel);
@@ -207,6 +238,7 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
         online.setOpaque(false);
         leftpane.setOpaque(false);
         leftpane.getViewport().setBackground(new Color(255, 255, 255, 150));
+
 
         Container pane = getContentPane();
         pane.add(topPanel, BorderLayout.NORTH);
@@ -263,6 +295,12 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
         worker.execute();
     }
 
+    private void sendOthersMessage(String targerUser) throws IOException {
+        ChatClientAPI chatClientAPI = new ChatClientAPIImpl("localhost", 8888);
+        ChatWithUserMessage chatWithUserMessage = new ChatWithUserMessage(GlobalData.getUID(), messageSent, targerUser);
+        chatClientAPI.sendUserMessage(chatWithUserMessage);
+    }
+
     private String getWaitingMessage() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < dotCount; i++) {
@@ -271,27 +309,83 @@ public class ChatFrameUI extends JFrame implements ClientReceiver.MessageCallbac
         return sb.toString();
     }
 
+    @Override
+    public void onMessageReceived(ChatMessage message) {
+        // 在这里处理收到的消息
+//        System.out.println("Received message: " + message);
+        chathistory.append(message.getRole() + ": " + message.getContent() + "\n");
+    }
+
     public void startReceivingMessages() {
         ClientReceiver clientReceiver;
         try {
-            clientReceiver = new ClientReceiver(this); // 将当前 UI 文件实例作为回调接口
+            clientReceiver = new ClientReceiver(this);  // 将当前 UI 文件实例作为回调接口
             clientReceiver.startReceiving();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    @Override
-    public void onMessageReceived(ChatMessage message) {
-        System.out.println("Received message: " + message);
-        chathistory.append("用户: " + message + "\n");
+
+    public void startTimer() {
+        timer.start();
     }
 
-//    public void testSend(String toUserId) throws IOException {
-//        ChatWithUserMessage chatWithUserMessage = new ChatWithUserMessage(
-//                "你好", toUserId);
-//        ChatClientAPI chatClientAPI = new ChatClientAPIImpl("localhost", 8888);
-//        chatClientAPI.sendUserMessage(chatWithUserMessage);
-//        System.out.println("Perfectly sent!");
+    public void stopTimer() {
+        timer.stop();
+    }
+//    public void startReceivingOnline() {
+//        ChatFrameUI currentUI = this;
+//        try {
+//            ClientOnlineReceiver clientReceiver = new ClientOnlineReceiver(new ClientOnlineReceiver.MessageCallback() {
+//                @Override
+//                public void onMessageReceived(OnlineListRespMessage message) {
+//                    List<String>allOnline = message.getOnlineList();
+//                    for (String name : allOnline) {
+//                        currentUI.friends.addItem(name);
+//                    }
+//                }
+//            });
+//            clientReceiver.startReceiving();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 //    }
+
+    public void showAllOnlineList() throws IOException {
+        ChatClientAPI chatClientAPI = new ChatClientAPIImpl("localhost", 8888);
+        UniqueMessage uniqueMessage = new UniqueMessage("yes");
+        List<String> allOnline = chatClientAPI.getAllOnlineName(uniqueMessage);
+
+        // 比较序列和 JComboBox 中的元素是否相等
+        Boolean areEqual = areElementsEqual(allOnline, friends);
+
+        if (areEqual)
+            return;
+        else {
+            friends.removeAllItems();
+            online.setText("");
+            for (String name : allOnline) {
+                if (!Objects.equals(name, GlobalData.getUID())) {
+                    friends.addItem(name);
+                    online.append(name + "\n");
+                }
+            }
+        }
+
+    }
+
+    public static boolean areElementsEqual(List<String> sequence1, JComboBox<String> comboBox) {
+        // 将序列转换为集合
+        Set<String> set1 = new HashSet<>(sequence1);
+        Set<String> set2 = new HashSet<>();
+
+        // 将 JComboBox 中的元素添加到集合中
+        for (int i = 0; i < comboBox.getItemCount(); i++) {
+            set2.add(comboBox.getItemAt(i));
+        }
+
+        // 比较集合的大小和元素是否相等
+        return set1.size() == set2.size() && set1.containsAll(set2);
+    }
 }
